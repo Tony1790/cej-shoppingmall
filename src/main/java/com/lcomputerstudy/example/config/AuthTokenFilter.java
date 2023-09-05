9ee8534d76522077db0bd5255d24d2158a471ea9
ssfewf-serf <tony45081790@gmail.com>
2023-09-05 10:03:01 +0900

    Update AuthTokenFilter.java

--- src/main/java/com/lcomputerstudy/example/config/AuthTokenFilter.java
+++ src/main/java/com/lcomputerstudy/example/config/AuthTokenFilter.java
@@ -42,22 +42,31 @@ public class AuthTokenFilter extends OncePerRequestFilter {
 		return null;
 	}
 	
-	//
+	/*OncePerRequestFilter에서 오버라이드된 메소드.
+	 * parseJwt메소드 호출해서 유효성 검사한 후 토큰에서 username 추출.
+	 * 추출된 username으로 loadUserByUsername메소드 사용해 User객체를 생성한다.
+	 * 
+	*/
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
 		try {
+			//parseJwt 메소드 호출
 			String jwt = parseJwt(request);
 			
+			//추출한 토큰의 유효성 검사 후 true일 때, Username 추출
 			if(jwt != null && jwtUtils.validateJwtToken(jwt)) {
 				String username = jwtUtils.getUserNameFromJwtToken(jwt);
 				
 				UserDetails userDetials = userService.loadUserByUsername(username);
-				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-						userDetials, null, userDetials.getAuthorities());
+				
+				//UsernamePasswordAuthenticationToken객체를 생성한다. args로 Object principal, Object credentials, Collection<? extends GrantedAuthority를 요구한다.
+				//JWT인증에서 password는 필요없기 때문에 userDetails, userDetials.getAuthorities()만 필요하다.
+				UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetials, null, userDetials.getAuthorities());
 				
 				authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
 				
+				//현재 thread에 authentication을 연결해서 요청처리 중에 현재 인증된 사용자에 대한 정보에 접근이 가능하게 만듬.
 				SecurityContextHolder.getContext().setAuthentication(authentication);
 			}
 		} catch (Exception e) {
